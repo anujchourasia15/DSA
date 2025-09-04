a2da3445fd4f8a844c4b5a31281b428ef2f31cd3
Anuj Chourasia <anujchourasia15@gmail.com>
2025-09-05 00:26:29 +0530

    4 sep checkin

---
+++ Misc/findIndicesStableMountains.java
@@ -0,0 +1,14 @@
+// 4 sep 2025
+// https://leetcode.com/problems/find-indices-of-stable-mountains/
+
+class Solution {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 1; i < height.length; i++){
+            if(height[i-1] > threshold){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
